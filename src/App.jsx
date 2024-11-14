948ec9e97e717d399993fcde7d7efe2853a7fc79
Gaurav Verma <24f2004146@ds.study.iitm.gehu.ac.in>
2024-11-14 08:50:08 +0530

    Created: tailwind and react configuration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='text-3xl text-blue-400'>Namaste React</h1>
+    </div>
+  );
+}
+
+export default App;
